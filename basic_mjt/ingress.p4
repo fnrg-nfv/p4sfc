5f0e12b291f2336896d30b619c9e087b94915a5b
virgilma <virgilma28@gmail.com>
2019-11-29 23:58:09 +0800

    basic work!

---
+++ basic_mjt/ingress.p4
@@ -0,0 +1,153 @@
+/* -*- P4_16 -*- */
+#include <core.p4>
+#include <v1model.p4>
+
+const bit<16> TYPE_IPV4 = 0x800;
+const bit<16> TYPE_FOO = 0xcdf;  // customed type
+
+/*************************************************************************
+*********************** H E A D E R S  ***********************************
+*************************************************************************/
+
+typedef bit<9>  egressSpec_t;
+typedef bit<48> macAddr_t;
+typedef bit<32> ip4Addr_t;
+
+header ethernet_t {
+    macAddr_t dstAddr;
+    macAddr_t srcAddr;
+    bit<16>   etherType;
+}
+
+header foo_t {
+    bit<1>    flag;
+    bit<15>   offset;
+}
+
+struct metadata {
+    /* empty */
+}
+
+struct headers {
+    ethernet_t   ethernet;
+    foo_t        foo;
+}
+
+/*************************************************************************
+*********************** P A R S E R  ***********************************
+*************************************************************************/
+
+parser MyParser(packet_in packet,
+                out headers hdr,
+                inout metadata meta,
+                inout standard_metadata_t standard_metadata) {
+
+    state start {
+        transition parse_ethernet;
+    }
+
+    state parse_ethernet {
+        packet.extract(hdr.ethernet);
+        transition select(hdr.ethernet.etherType) {
+            TYPE_FOO: parse_foo;
+            default: accept;
+        }
+    }
+
+    state parse_foo {
+        packet.extract(hdr.foo);
+        transition accept;
+    }
+
+}
+
+/*************************************************************************
+************   C H E C K S U M    V E R I F I C A T I O N   *************
+*************************************************************************/
+
+control MyVerifyChecksum(inout headers hdr, inout metadata meta) {   
+    apply {  }
+}
+
+
+/*************************************************************************
+**************  I N G R E S S   P R O C E S S I N G   *******************
+*************************************************************************/
+
+control MyIngress(inout headers hdr,
+                  inout metadata meta,
+                  inout standard_metadata_t standard_metadata) {
+    action drop() {
+        mark_to_drop(standard_metadata);
+    }
+
+    action port_forward(macAddr_t dstAddr, egressSpec_t port) {
+        standard_metadata.egress_spec = port;
+        hdr.ethernet.srcAddr = hdr.ethernet.dstAddr;
+        hdr.ethernet.dstAddr = dstAddr;
+    }
+
+    table port_exact {
+        key = {
+            standard_metadata.ingress_port: exact;
+        }
+        actions = {
+            port_forward;
+            drop;
+            NoAction;
+        }
+        size = 1024;
+        default_action = drop();
+    }
+
+    action ingress_add_foo() {
+        hdr.foo.flag = 1;
+    }
+    
+    apply {
+        port_exact.apply();
+    }
+}
+
+/*************************************************************************
+****************  E G R E S S   P R O C E S S I N G   *******************
+*************************************************************************/
+
+control MyEgress(inout headers hdr,
+                 inout metadata meta,
+                 inout standard_metadata_t standard_metadata) {
+    apply {  }
+}
+
+/*************************************************************************
+*************   C H E C K S U M    C O M P U T A T I O N   **************
+*************************************************************************/
+
+control MyComputeChecksum(inout headers  hdr, inout metadata meta) {
+    apply {
+    }
+}
+
+/*************************************************************************
+***********************  D E P A R S E R  *******************************
+*************************************************************************/
+
+control MyDeparser(packet_out packet, in headers hdr) {
+    apply {
+        packet.emit(hdr.ethernet);
+        // packet.emit(hdr.foo);
+    }
+}
+
+/*************************************************************************
+***********************  S W I T C H  *******************************
+*************************************************************************/
+
+V1Switch(
+MyParser(),
+MyVerifyChecksum(),
+MyIngress(),
+MyEgress(),
+MyComputeChecksum(),
+MyDeparser()
+) main;
--- utils/run_exercise.py
+++ utils/run_exercise.py
@@ -19,18 +19,25 @@
 # We encourage you to dissect this script to better understand the BMv2/Mininet
 # environment used by the P4 tutorial.
 #
-import os, sys, json, subprocess, re, argparse
+import os
+import sys
+import json
+import subprocess
+import re
+import argparse
 from time import sleep
 
 from p4_mininet import P4Switch, P4Host
 
-from mininet.net import Mininet
+from mininet.net import Mininet, Containernet
 from mininet.topo import Topo
 from mininet.link import TCLink
+from mininet.node import Docker
 from mininet.cli import CLI
 
 from p4runtime_switch import P4RuntimeSwitch
 import p4runtime_lib.simple_controller
+
 
 def configureP4Switch(**switch_args):
     """ Helper class that is called by mininet to initialize
@@ -51,6 +58,7 @@ def configureP4Switch(**switch_args):
     else:
         class ConfiguredP4Switch(P4Switch):
             next_thrift_port = 9090
+
             def __init__(self, *opts, **kwargs):
                 global next_thrift_port
                 kwargs.update(switch_args)
@@ -64,9 +72,22 @@ def configureP4Switch(**switch_args):
         return ConfiguredP4Switch
 
 
+def configureDockerHost(**host_args):
+    class ConfiguredDockerHost(Docker):
+        def __init__(self, name, **kwargs):
+            kwargs.update(host_args)
+            Docker.__init__(self, name, **kwargs)
+
+        def describe(self):
+            print "%s -> dimage: %s" % (self.name, self.dimage)
+
+    return ConfiguredDockerHost
+
+
 class ExerciseTopo(Topo):
     """ The mininet topology class for the P4 tutorial exercises.
     """
+
     def __init__(self, hosts, switches, links, log_dir, bmv2_exe, pcap_dir, **opts):
         Topo.__init__(self, **opts)
         host_links = []
@@ -82,21 +103,24 @@ class ExerciseTopo(Topo):
         for sw, params in switches.iteritems():
             if "program" in params:
                 switchClass = configureP4Switch(
-                        sw_path=bmv2_exe,
-                        json_path=params["program"],
-                        log_console=True,
-                        pcap_dump=pcap_dir)
+                    sw_path=bmv2_exe,
+                    json_path=params["program"],
+                    log_console=True,
+                    pcap_dump=pcap_dir)
             else:
                 # add default switch
                 switchClass = None
-            self.addSwitch(sw, log_file="%s/%s.log" %(log_dir, sw), cls=switchClass)
+            self.addSwitch(sw, log_file="%s/%s.log" %
+                           (log_dir, sw), cls=switchClass)
 
         for link in host_links:
             host_name = link['node1']
             sw_name, sw_port = self.parse_switch_node(link['node2'])
             host_ip = hosts[host_name]['ip']
             host_mac = hosts[host_name]['mac']
-            self.addHost(host_name, ip=host_ip, mac=host_mac)
+            host_image = hosts[host_name]['image']
+            self.addHost(host_name, dimage=host_image,
+                         ip=host_ip, mac=host_mac)
             self.addLink(host_name, sw_name,
                          delay=link['latency'], bw=link['bandwidth'],
                          port2=sw_port)
@@ -105,9 +129,8 @@ class ExerciseTopo(Topo):
             sw1_name, sw1_port = self.parse_switch_node(link['node1'])
             sw2_name, sw2_port = self.parse_switch_node(link['node2'])
             self.addLink(sw1_name, sw2_name,
-                        port1=sw1_port, port2=sw2_port,
-                        delay=link['latency'], bw=link['bandwidth'])
-
+                         port1=sw1_port, port2=sw2_port,
+                         delay=link['latency'], bw=link['bandwidth'])
 
     def parse_switch_node(self, node):
         assert(len(node.split('-')) == 2)
@@ -115,7 +138,8 @@ class ExerciseTopo(Topo):
         try:
             sw_port = int(sw_port[1])
         except:
-            raise Exception('Invalid switch node in topology file: {}'.format(node))
+            raise Exception(
+                'Invalid switch node in topology file: {}'.format(node))
         return sw_name, sw_port
 
 
@@ -137,6 +161,7 @@ class ExerciseRunner:
             net : Mininet object // The mininet instance
 
     """
+
     def logger(self, *items):
         if not self.quiet:
             print(' '.join(items))
@@ -148,9 +173,8 @@ class ExerciseRunner:
         else:
             return str(l) + "ms"
 
-
     def __init__(self, topo_file, log_dir, pcap_dir,
-                       switch_json, bmv2_exe='simple_switch', quiet=False):
+                 switch_json, bmv2_exe='simple_switch', quiet=False):
         """ Initializes some attributes and reads the topology json. Does not
             actually run the exercise. Use run_exercise() for that.
 
@@ -176,13 +200,13 @@ class ExerciseRunner:
         for dir_name in [log_dir, pcap_dir]:
             if not os.path.isdir(dir_name):
                 if os.path.exists(dir_name):
-                    raise Exception("'%s' exists and is not a directory!" % dir_name)
+                    raise Exception(
+                        "'%s' exists and is not a directory!" % dir_name)
                 os.mkdir(dir_name)
         self.log_dir = log_dir
         self.pcap_dir = pcap_dir
         self.switch_json = switch_json
         self.bmv2_exe = bmv2_exe
-
 
     def run_exercise(self):
         """ Sets up the mininet instance, programs the switches,
@@ -205,7 +229,6 @@ class ExerciseRunner:
         # stop right after the CLI is exited
         self.net.stop()
 
-
     def parse_links(self, unparsed_links):
         """ Given a list of links descriptions of the form [node1, node2, latency, bandwidth]
             with the latency and bandwidth being optional, parses these descriptions
@@ -216,23 +239,23 @@ class ExerciseRunner:
             # make sure each link's endpoints are ordered alphabetically
             s, t, = link[0], link[1]
             if s > t:
-                s,t = t,s
+                s, t = t, s
 
-            link_dict = {'node1':s,
-                        'node2':t,
-                        'latency':'0ms',
-                        'bandwidth':None
-                        }
+            link_dict = {'node1': s,
+                         'node2': t,
+                         'latency': '0ms',
+                         'bandwidth': None
+                         }
             if len(link) > 2:
                 link_dict['latency'] = self.format_latency(link[2])
             if len(link) > 3:
                 link_dict['bandwidth'] = link[3]
 
             if link_dict['node1'][0] == 'h':
-                assert link_dict['node2'][0] == 's', 'Hosts should be connected to switches, not ' + str(link_dict['node2'])
+                assert link_dict['node2'][0] == 's', 'Hosts should be connected to switches, not ' + str(
+                    link_dict['node2'])
             links.append(link_dict)
         return links
-
 
     def create_network(self):
         """ Create the mininet network object, and store it as self.net.
@@ -244,18 +267,23 @@ class ExerciseRunner:
         self.logger("Building mininet topology.")
 
         defaultSwitchClass = configureP4Switch(
-                                sw_path=self.bmv2_exe,
-                                json_path=self.switch_json,
-                                log_console=True,
-                                pcap_dump=self.pcap_dir)
+            sw_path=self.bmv2_exe,
+            json_path=self.switch_json,
+            log_console=True,
+            pcap_dump=self.pcap_dir)
 
-        self.topo = ExerciseTopo(self.hosts, self.switches, self.links, self.log_dir, self.bmv2_exe, self.pcap_dir)
+        self.topo = ExerciseTopo(
+            self.hosts, self.switches, self.links, self.log_dir, self.bmv2_exe, self.pcap_dir)
 
-        self.net = Mininet(topo = self.topo,
-                      link = TCLink,
-                      host = P4Host,
-                      switch = defaultSwitchClass,
-                      controller = None)
+        defaultHostClass = configureDockerHost(
+            volumes=["/dev/hugepages:/dev/hugepages:rw"])
+
+        self.net = Containernet(topo=self.topo,
+                                link=TCLink,
+                                # host=P4Host,
+                                host=defaultHostClass,
+                                switch=defaultSwitchClass,
+                                controller=None)
 
     def program_switch_p4runtime(self, sw_name, sw_dict):
         """ This method will use P4Runtime to program the switch using the
@@ -265,9 +293,10 @@ class ExerciseRunner:
         grpc_port = sw_obj.grpc_port
         device_id = sw_obj.device_id
         runtime_json = sw_dict['runtime_json']
-        self.logger('Configuring switch %s using P4Runtime with file %s' % (sw_name, runtime_json))
+        self.logger('Configuring switch %s using P4Runtime with file %s' %
+                    (sw_name, runtime_json))
         with open(runtime_json, 'r') as sw_conf_file:
-            outfile = '%s/%s-p4runtime-requests.txt' %(self.log_dir, sw_name)
+            outfile = '%s/%s-p4runtime-requests.txt' % (self.log_dir, sw_name)
             p4runtime_lib.simple_controller.program_switch(
                 addr='127.0.0.1:%d' % grpc_port,
                 device_id=device_id,
@@ -285,9 +314,10 @@ class ExerciseRunner:
         thrift_port = sw_obj.thrift_port
 
         cli_input_commands = sw_dict['cli_input']
-        self.logger('Configuring switch %s with file %s' % (sw_name, cli_input_commands))
+        self.logger('Configuring switch %s with file %s' %
+                    (sw_name, cli_input_commands))
         with open(cli_input_commands, 'r') as fin:
-            cli_outfile = '%s/%s_cli_output.log'%(self.log_dir, sw_name)
+            cli_outfile = '%s/%s_cli_output.log' % (self.log_dir, sw_name)
             with open(cli_outfile, 'w') as fout:
                 subprocess.Popen([cli, '--thrift-port', str(thrift_port)],
                                  stdin=fin, stdout=fout)
@@ -311,7 +341,6 @@ class ExerciseRunner:
             if "commands" in host_info:
                 for cmd in host_info["commands"]:
                     h.cmd(cmd)
-
 
     def do_net_cli(self):
         """ Starts up the mininet CLI and prints some helpful output.
@@ -341,15 +370,17 @@ class ExerciseRunner:
             print('  simple_switch_CLI --thrift-port <switch thrift port>')
             print('')
         print('To view a switch log, run this command from your host OS:')
-        print('  tail -f %s/<switchname>.log' %  self.log_dir)
+        print('  tail -f %s/<switchname>.log' % self.log_dir)
         print('')
-        print('To view the switch output pcap, check the pcap files in %s:' % self.pcap_dir)
+        print('To view the switch output pcap, check the pcap files in %s:' %
+              self.pcap_dir)
         print(' for example run:  sudo tcpdump -xxx -r s1-eth1.pcap')
         print('')
         if 'grpc' in self.bmv2_exe:
             print('To view the P4Runtime requests sent to the switch, check the')
             print('corresponding txt file in %s:' % self.log_dir)
-            print(' for example run:  cat %s/s1-p4runtime-requests.txt' % self.log_dir)
+            print(' for example run:  cat %s/s1-p4runtime-requests.txt' %
+                  self.log_dir)
             print('')
 
         CLI(self.net)
@@ -364,11 +395,13 @@ def get_args():
                         action='store_true', required=False, default=False)
     parser.add_argument('-t', '--topo', help='Path to topology json',
                         type=str, required=False, default='./topology.json')
-    parser.add_argument('-l', '--log-dir', type=str, required=False, default=default_logs)
-    parser.add_argument('-p', '--pcap-dir', type=str, required=False, default=default_pcaps)
+    parser.add_argument('-l', '--log-dir', type=str,
+                        required=False, default=default_logs)
+    parser.add_argument('-p', '--pcap-dir', type=str,
+                        required=False, default=default_pcaps)
     parser.add_argument('-j', '--switch_json', type=str, required=False)
     parser.add_argument('-b', '--behavioral-exe', help='Path to behavioral executable',
-                                type=str, required=False, default='simple_switch')
+                        type=str, required=False, default='simple_switch')
     return parser.parse_args()
 
 
@@ -381,4 +414,3 @@ if __name__ == '__main__':
                               args.switch_json, args.behavioral_exe, args.quiet)
 
     exercise.run_exercise()
-
