3718f17e701abcf099ea233c5c3c525f02348cab
virgilma <virgilma28@gmail.com>
2020-04-17 11:53:23 +0800

    add cpp-httplib submodule

---
+++ http-test/client.cc
@@ -0,0 +1,17 @@
+#include <httplib.h>
+#include <iostream>
+
+int main(void)
+{
+  // IMPORTANT: 1st parameter must be a hostname or an IP adress string.
+  httplib::Client cli("localhost", 1234);
+
+  auto res = cli.Get("/hi");
+  if (res && res->status == 200) {
+    std::cout << res->body << std::endl;
+  }
+  res = cli.Get("/stop");
+  if (res && res->status == 200) {
+    std::cout << res->body << std::endl;
+  }
+}
