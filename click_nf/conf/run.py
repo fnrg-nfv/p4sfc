6249253ef4cd75127ad9a77b64f58b66cfd40602
virgilma <virgilma28@gmail.com>
2020-04-24 19:01:55 +0800

    update run.py

--- click_nf/conf/run.py
+++ click_nf/conf/run.py
@@ -7,7 +7,9 @@ def getargs():
     parser = argparse.ArgumentParser()
     parser.add_argument('file',  help="click router pattern file")
     parser.add_argument("-i", "--id", type=int, help="instance id for NF")
-    parser.add_argument('nargs', nargs='+', help="pattern go")
+    parser.add_argument('-d', '--debug', action='store_true',
+                        help='output result file for debugging')
+    parser.add_argument('nargs', nargs='+', help="pattern variables")
     args = parser.parse_args()
     return args
 
@@ -17,8 +19,8 @@ if __name__ == "__main__":
     f = open(args.file)
     s = f.read()
 
-    if args.id:
-        s.replace('$id', str(args.id))
+    if "id" in args:
+        s = s.replace('$id', str(args.id))
 
     i = 1
     for arg in args.nargs:
@@ -26,7 +28,10 @@ if __name__ == "__main__":
         s = s.replace(variable, arg)
         i += 1
     cmd = "click -e \"" + s + '\"'
-    # print(cmd)
 
-    result = os.popen(cmd).read()
-    print(result)
+    if args.debug:
+        print(cmd)
+
+    os.system(cmd)
+    # result = os.popen(cmd).read()
+    # print(result)
