6795b116a8a433fed0a4300b3a89c437097a7fae
virgilma <virgilma28@gmail.com>
2020-05-26 15:33:07 +0800

    add custom header modules

--- click_nf/package/ips.cc
+++ click_nf/package/ips.cc
@@ -22,9 +22,9 @@ void SampleIPS::parse_pattern(String &s) {
     if (c >= '0' && c <= '9')
       c -= '0';
     else if (c >= 'a' && c <= 'f')
-      c -= 'a' + 10;
+      c = c - 'a' + 10;
     else if (c >= 'A' && c <= 'F')
-      c -= 'A' + 10;
+      c = c - 'A' + 10;
     else
       continue;
 
